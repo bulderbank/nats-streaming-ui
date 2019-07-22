d7510e1a11588eeaf3c3f53cc0477da43f1e2933
Hans Kristian Flaatten <hans.flaatten@evry.com>
2019-07-22 12:29:56 +0200

    Move nats structs to a sparate file for better readability

---
+++ models.go
@@ -0,0 +1,59 @@
+package main
+
+type natsSubscription struct {
+	ClientId     string `json:"client_id"`
+	Inbox        string `json:"inbox"`
+	AckInbox     string `json:"ack_inbox"`
+	QueueName    string `json:"queue_name"`
+	IsDurable    bool   `json:"is_durable"`
+	IsOffline    bool   `json:"is_offline"`
+	MaxInflight  int    `json:"max_inflight"`
+	AckWait      int    `json:"ack_wait"`
+	LastSent     int    `json:"last_sent"`
+	PendingCount int    `json:"pending_count"`
+	IsStalled    bool   `json:"is_stalled"`
+}
+
+// IsHealthy returns true if the subscription is concidered healthy. This is
+// comprised by the other facts that we know about the subscription.
+func (sub natsSubscription) IsHealthy() bool {
+	return !sub.IsOffline && !sub.IsStalled && sub.PendingCount == 0
+}
+
+type natsChannel struct {
+	Name          string             `json:"name"`
+	MessagesCount int                `json:"msgs"`
+	BytesCount    int                `json:"bytes"`
+	FirstSequence int                `json:"first_seq"`
+	LastSequence  int                `json:"last_seq"`
+	Subscriptions []natsSubscription `json:"subscriptions"`
+}
+
+// Color returns a string with the color code for the status of a given channel.
+// This is based on the IsHealthy status from the corresponding subscriptions.
+func (ch natsChannel) Color() string {
+	color := "green"
+
+	if len(ch.Subscriptions) == 0 {
+		color = "orange"
+	}
+
+	for _, sub := range ch.Subscriptions {
+		if !sub.IsHealthy() {
+			color = "red"
+		}
+	}
+
+	return color
+}
+
+type natsChannels struct {
+	ClusterId string        `json:"cluster_id"`
+	ServerId  string        `json:"server_id"`
+	Timestamp string        `json:"now"`
+	Offset    int           `json:"offset"`
+	Limit     int           `json:"limit"`
+	Count     int           `json:"count"`
+	Total     int           `json:"total"`
+	Channels  []natsChannel `json:"channels"`
+}
